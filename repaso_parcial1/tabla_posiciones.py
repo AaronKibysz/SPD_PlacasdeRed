aae9b5845ec7571482a221a803998677962a9a06
Ignacio Audero <nachhoaudero@gmail.com>
2023-10-12 08:53:13 -0300

    asd

--- repaso_parcial1/tabla_posiciones.py
+++ repaso_parcial1/tabla_posiciones.py
@@ -1,12 +1,12 @@
 print('TABLA DE POSICIONES FASE DE GRUPOS')
 
-list_teams = []
+list_teams = [] # Lista para guardar los equipos
 
-for i in range(1, 5):
+for i in range(1, 5): # En este bucle for se ingresaran los nombres de los equipos que integran el grupo
     team = input(f'Ingese el equipo {i}: ')
     list_teams.append(team)
 
-print('Los equipos que integran el grupo son:')
+print('\nLos equipos que integran el grupo son:') # Se muestra en pantalla los equipos ingresados anteriormente
 for i in range(4):
     print(f'{i + 1}- {list_teams[i]}')
 
