fc7ef3a7d7b485fdac48051793a2922dc9fae4d6
AaronKibysz <aaronkibysz@gmail.com>
2023-10-17 14:40:57 -0300

    Commit easteregg

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 document.getElementById('boton').onclick = function(){
   alert('Aciar Nahuel\nAudero Ignacio\nKibysz Aaron\nMartinez Chiappetta Francisco')
 }
+function mostrarGif(){
+  const gif = document.getElementById('gif');
+  gif.style.display = 'block'
+
+  const gifImage = document.getElementById('gifImage')
+  gifImage.addEventListener('click', function(){
+    gif.style.display = 'none';
+  });
+}
