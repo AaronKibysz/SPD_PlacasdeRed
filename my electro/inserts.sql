6504d83caba6ba8633dd6f98d48d54f5965ee6e0
Francisco <franciscomartinezchiappetta@gmail.com>
2023-11-14 16:31:49 -0300

    inserts

--- my electro/inserts.sql
+++ my electro/inserts.sql
@@ -16,4 +16,5 @@ INSERT INTO Dispositivos(Descripcion, Precio, Stock, ComponenteID)
 VALUES
     ('Pci-e Wifi Tp-link 1 Antena Wn781nd', 13.999, 95, 1),
     ('Wifi Adaptador Usb NOGA 600mbs', 19.999, 62, 1),
-    ('')
+    ('Nvidia MSI Ventus GeForce RTX 3070 8gb', 499.900, 20, 2),
+    
